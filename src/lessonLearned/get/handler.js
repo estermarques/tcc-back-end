e4b0eac893c718b296680ffce85a4ef13f1fc00a
Ester Marques <estermarques@id.uff.br>
2022-09-23 15:53:28 -0300

    get and list leasson learned

---
+++ src/lessonLearned/get/handler.js
@@ -0,0 +1,48 @@
+import db from '../../../models';
+
+/**
+ * @name ListLessonsLearned
+ * @command serverless invoke local -f ListLessonsLearned -p src/lessonLearned/list/mock.json
+ */
+export async function main(event) {
+  const { projectId, id } = event.queryStringParameters;
+  const body = {};
+  let statusCode;
+
+  try {
+    const {
+      lessonLearned
+    } = db;
+
+    let lessonsLearned;
+
+    if (projectId && projectId !== '') {
+      lessonsLearned = await lessonLearned.findAll({
+        where: {
+          projectId
+        }
+      });
+    } else {
+      lessonsLearned = await lessonLearned.findByPk(id);
+    }
+
+    statusCode = 200;
+    body.message = "Success to list lessons learned";
+    body.lessonsLearned = lessonsLearned;
+
+  } catch (error) {
+    console.log(error);
+    statusCode = 500;
+    body.error = 'Error to find lessons learned';
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify(body),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Headers': 'X-Amz-Security-Token',
+    }
+  };
+}
